ca68c924447ce3177d3599cf5e00e3b020c0002e
Nithin Kumar <super0blue0@gmail.com>
2024-02-21 12:44:37 -0800

    console

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
     // req.body will hold the text fields, if there were any
 
     console.log(req); // You can use this to see file information
+    console.log(req.body.file); // You can use this to see file information
 
     res.json({ message: 'File uploaded successfully' });
 });
